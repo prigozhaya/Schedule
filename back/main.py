8d70d61045970f32936f70af3b4ac6b956c16a6d
Prigozhaya Liaz <elizavetaprigozhaya@gmail.com>
2023-12-22 12:06:27 +0300

    feat: add teacher schedule query

--- back/main.py
+++ back/main.py
@@ -6,49 +6,20 @@ from datetime import timedelta, datetime
 
 import pyodbc
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 global faculcyId, educationForm, groupId
 
 
 app = Flask(__name__)
+CORS(app)
 
-# Получаем от клиента Факультет и Форму обучения, которые потребуются для извлечения данных из SMSS
-@app.route('/recieveGroups', methods=["POST"])
-def getFaculcyId():
-    faculcyId = request.json.get("facultyId")
-    educationForm = request.json.get("educationForm")
-
-    return jsonify({'message': 'Получен id факультета'}), 200
-
-
-
-
-
-# Пользователь нажал кнопку, ему надо вернуть
-@app.route('/buttonClicked', methods=["POST"])
-def buttonClicked():
-    faculcyId = request.json.get("facultyId")
-    educationForm = request.json.get("educationForm")
-    try:
-        connectionString = f'DRIVER={{SQL Server}};' \
-                           f'SERVER={constants.SERVER};' \
-                           f'UID={constants.USERNAME};' \
-                           f'PWD={constants.PASSWORD}'
-        conn = pyodbc.connect(connectionString)
-        print('Succesfull connected')
-        cursor = conn.cursor()
-        cursor.execute(queries.GET_GROUP_QUERY)
-        result = cursor.fetchall()
-        return jsonify(result)
-    except pyodbc.Error as error:
-        print(f'An error {error} occured')
-    finally:
-        cursor.close()
-        conn.close()
-        print("Connection closed")
-
-@app.route('/groupName', methods=["GET"])
-def getGroupId():
-    return jsonify(groupId)
+# # Получаем от клиента Факультет и Форму обучения, которые потребуются для извлечения данных из SMSS
+# @app.route('/recieveGroups', methods=["GET"]) # УЭЭЭЭ изменить POST на GET
+# def getFaculcyId():
+#     faculcyId = request.args.get("facultyId")           # УЭЭЭЭ изменить json на args
+#     educationForm = request.args.get("educationForm")
+#
+#     return jsonify({'message': 'Получен id факультета'}), 200
 
 def data_from_db_to_client(cursor, query, *args):
     cursor.execute(query.format(*args))
@@ -59,13 +30,105 @@ def data_from_db_to_client(cursor, query, *args):
         json_data.append(dict(zip(row_headers, result)))
     return json.dumps(json_data, ensure_ascii=False, default=str)
 
+
+
+#                                                    http://127.0.0.1:5000/buttonClicked?facultyId=222&educationForm=1      # УЭЭЭЭ
+# Пользователь нажал кнопку, ему надо вернуть
+@app.route('/buttonClicked', methods=["GET"])
+def buttonClicked():
+    faculcyId = request.args.get("facultyId")
+    educationForm = request.args.get("educationForm")
+    try:
+        connectionString = f'DRIVER={{SQL Server}};' \
+                           f'SERVER={constants.SERVER};' \
+                           f'UID={constants.USERNAME};' \
+                           f'PWD={constants.PASSWORD}'
+        conn = pyodbc.connect(connectionString)
+        print('Succesfull connected')
+        cursor = conn.cursor()
+        cursor.execute(f"USE {constants.DATABASE}")
+        result = data_from_db_to_client(cursor, queries.GET_GROUP_QUERY, faculcyId, educationForm)
+        return app.response_class(
+                response=json.dumps(result),
+                status=200,
+                mimetype='application/json'
+        )
+    except pyodbc.Error as error:
+        print(f'An error {error} occured')
+    finally:
+        cursor.close()
+        conn.close()
+        print("Connection closed")
+
+#                                                                                               Выгрузка ФИО преподов
+@app.route('/getTeacherNames')
+def getTeacherNames():
+    try:
+        connectionString = f'DRIVER={{SQL Server}};' \
+                           f'SERVER={constants.SERVER};' \
+                           f'UID={constants.USERNAME};' \
+                           f'PWD={constants.PASSWORD}'
+        conn = pyodbc.connect(connectionString)
+        print('Succesfull connected')
+        cursor = conn.cursor()
+        cursor.execute(f"USE {constants.DATABASE}")
+        result = data_from_db_to_client(cursor, queries.GET_TEACHERS_NAME_QUERY)
+        return json.dumps(result)
+    except pyodbc.Error as error:
+        print(f'An error {error} occured')
+    finally:
+        cursor.close()
+        conn.close()
+        print("Connection closed")
+
+#                                                    http://127.0.0.1:5000/teachers?FIO_teacher={"доц. Бартош Наталья Николаевна"}      # УЭЭЭЭ
+# 2023-12-18 2023-12-24
+@app.route('/teachers', methods=["GET"])
+def getTeachersSchedule():
+    currentMonday, currentSunday = setWeek(request)
+    FIO_teacher = request.args.get("FIO_teacher")
+    try:
+        connectionString = f'DRIVER={{SQL Server}};' \
+                           f'SERVER={constants.SERVER};' \
+                           f'UID={constants.USERNAME};' \
+                           f'PWD={constants.PASSWORD}'
+        conn = pyodbc.connect(connectionString)
+        print('Succesfull connected')
+        cursor = conn.cursor()
+        cursor.execute(f"USE {constants.DATABASE}")
+        # FIO_teacher = 'доц. Бартош Наталья Николаевна' #!
+        result = data_from_db_to_client(cursor, queries.EXTRACT_DATA_FOR_TEACHERS_QUERY, currentMonday, currentSunday, FIO_teacher)
+        return json.dumps(result)
+    except pyodbc.Error as error:
+        print(f'An error {error} occured')
+    finally:
+        cursor.close()
+        conn.close()
+        print("Connection closed")
+
+def setWeek(request):
+    additionalTime = timedelta(days=0)
+    try:
+        weekType = request.args.get("weekType")
+    except TypeError as error:
+        print(f'An error {error} occured')
+    if weekType == "currentWeek":
+        additionalTime = timedelta(days=0)
+    elif weekType == "nextWeek":
+        additionalTime = timedelta(days=7)
+    now = datetime.now().date()
+    currentMonday = now - timedelta(days=now.weekday()) + additionalTime
+    currentSunday = (currentMonday + timedelta(days=5) + additionalTime).strftime("%Y-%m-%d")
+    currentMonday = currentMonday.strftime("%Y-%m-%d")
+    return currentMonday, currentSunday
+
+
+#                                           http://127.0.0.1:5000/?groupId=224003249    # УЭЭЭЭ
 @app.route('/')
 def main():
     # faculcyId, formaTimeId, dayBegin, dayEnd, groupId = 222, 1, 23, 28, 224003249 #!
-    now = datetime.now().date()
-    currentMonday = now - timedelta(days=now.weekday())
-    currentSunday = (currentMonday + timedelta(days=5)).strftime("%Y-%m-%d")
-    currentMonday = currentMonday.strftime("%Y-%m-%d")
+    currentMonday, currentSunday = setWeek(request)
+    groupId = request.args.get("groupId")
 
     connectionString = f'DRIVER={{SQL Server}};' \
                        f'SERVER={constants.SERVER};' \
@@ -76,7 +139,11 @@ def main():
     cursor.execute(f"USE {constants.DATABASE}")
     result = data_from_db_to_client(cursor, queries.EXTRACT_DATA_QUERY, currentMonday, currentSunday, groupId)
     print('nice')
-    return json.dumps(result)
+    return app.response_class(
+        response=json.dumps(result),
+        status=200,
+        mimetype='application/json'
+    )
 
 
 if __name__== "__main__":
--- back/queries.py
+++ back/queries.py
@@ -1,10 +1,10 @@
-GET_GROUP_QUERY = """
+GET_GROUP_QUERY = f"""
     SELECT Name, IdGroup
     FROM Groups
-    WHERE IdF = {0} AND Year > YEAR(GETDATE())-5 AND Name != 'арх' AND Name != 'а/о' AND IdFormaTime = {1}
+    WHERE IdF = {{0}} AND Year > YEAR(GETDATE())-5 AND Name != 'арх' AND Name != 'а/о' AND IdFormaTime = {{1}}
 """
 
-EXTRACT_DATA_QUERY = """
+EXTRACT_DATA_QUERY = f"""
     SELECT IdSchedule, DateIn, DateOut, sch.ScheduleDaysOptionV.Name2 as "Day",sch.ScheduleV.IdScheduleDaysOption as "DayNumber", sch.ScheduleV.IdScheduleTimeOption as "Lesson", sch.SubjectTime.TimeIn, sch.SubjectTime.TimeOut, dbo.Lesson.ShortName  as "Discipline",  dbo.ScPlanLessonType_V.ShortName  as "Discipline_Type", sch.LectureHallV.Number as "Сlassroom", Groups.Name as "Group", sch.ScheduleV.IdGroup,  Groups.IdF as Faculty, (NamePost + ' ' + NameF + ' ' + LEFT(NameI, 1)+'.' + LEFT(NameO, 1)+'.') AS FIO_teacher
     FROM sch.ScheduleV
     JOIN dbo.e_Teacher ON sch.ScheduleV.IdTeacher = dbo.e_Teacher.IdTeacher
@@ -15,6 +15,30 @@ EXTRACT_DATA_QUERY = """
     JOIN sch.LectureHallV ON sch.ScheduleV.IdLectureHall = sch.LectureHallV.IdLectureHall
     JOIN sch.ScheduleDaysOptionV ON sch.ScheduleV.IdScheduleDaysOption = sch.ScheduleDaysOptionV.IdScheduleDaysOption
     JOIN sch.SubjectTime ON sch.ScheduleV.IdScheduleTimeOption = sch.SubjectTime.IdScheduleTimeOption
-    WHERE DateIn <= '{0}' AND DateOut >= '{1}' AND Groups.IdGroup = {2}
-    ORDER BY sch.ScheduleV.IdScheduleDaysOption, Lesson;
+    WHERE DateIn <= '{{0}}' AND DateOut >= '{{1}}' AND Groups.IdGroup = {{2}}
+    ORDER BY sch.ScheduleV.IdScheduleDaysOption, Lesson 
+"""
+
+GET_TEACHERS_NAME_QUERY = f"""
+    SELECT DISTINCT (NamePost + ' ' + NameF + ' ' + NameI + ' ' + NameO) AS FIO_teacher
+    FROM sch.ScheduleV
+    JOIN dbo.e_Teacher ON sch.ScheduleV.IdTeacher = dbo.e_Teacher.IdTeacher
+    WHERE (NamePost + ' ' + NameF + ' ' + NameI + ' ' + NameO) IS NOT NULL
+    ORDER BY (NamePost + ' ' + NameF + ' ' + NameI + ' ' + NameO)
+"""
+
+EXTRACT_DATA_FOR_TEACHERS_QUERY = f"""
+    SELECT sch.ScheduleDaysOptionV.Name2 as "Day", sch.ScheduleV.IdScheduleDaysOption as "DayNumber", dbo.Lesson.ShortName  as "Discipline", sch.ScheduleV.IdScheduleTimeOption as "Lesson", sch.SubjectTime.TimeIn, sch.SubjectTime.TimeOut, dbo.ScPlanLessonType_V.ShortName  as "Discipline_Type", sch.LectureHallV.Number as "Сlassroom", Groups.Name as "Group", sch.ScheduleV.IdGroup,  Groups.IdF as Faculty, DateIn, DateOut, IdSchedule
+    FROM sch.ScheduleV
+    JOIN dbo.e_Teacher ON sch.ScheduleV.IdTeacher = dbo.e_Teacher.IdTeacher
+    JOIN Groups ON sch.ScheduleV.IdGroup = Groups.IdGroup
+    JOIN ScPlan ON sch.ScheduleV.IdPlan = ScPlan.IdPlan and sch.ScheduleV.IdScPlan = ScPlan.IdScPlan
+    JOIN dbo.Lesson ON ScPlan.IdLesson = dbo.Lesson.IdLesson
+    JOIN ScPlanLessonType_V ON sch.ScheduleV.IdScPlanLessonType = dbo.ScPlanLessonType_V.IdScPlanLessonType
+    JOIN sch.LectureHallV ON sch.ScheduleV.IdLectureHall = sch.LectureHallV.IdLectureHall
+    JOIN sch.ScheduleDaysOptionV ON sch.ScheduleV.IdScheduleDaysOption = sch.ScheduleDaysOptionV.IdScheduleDaysOption
+    JOIN sch.SubjectTime ON sch.ScheduleV.IdScheduleTimeOption = sch.SubjectTime.IdScheduleTimeOption
+    WHERE DateIn <= '{{0}}' AND DateOut >= '{{1}}' AND (NamePost + ' ' + NameF + ' ' + NameI + ' ' + NameO) = '{{2}}'
+    GROUP BY (NamePost + ' ' + NameF + ' ' + NameI + ' ' + NameO), sch.ScheduleDaysOptionV.Name2, sch.ScheduleV.IdScheduleDaysOption, IdSchedule, DateIn, DateOut, sch.ScheduleV.IdScheduleTimeOption, sch.SubjectTime.TimeIn, sch.SubjectTime.TimeOut, dbo.Lesson.ShortName, dbo.ScPlanLessonType_V.ShortName, sch.LectureHallV.Number, Groups.Name, sch.ScheduleV.IdGroup, Groups.IdF
+    ORDER BY (NamePost + ' ' + NameF + ' ' + NameI + ' ' + NameO), DayNumber, Lesson 
 """
